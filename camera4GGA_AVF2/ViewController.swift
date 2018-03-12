ec928f87c35b556e537b8600b2b671ea7fc76921
ctak <ctakahashi737@gmail.com>
2018-03-12 16:55:19 +0900

    Initial Commit

---
+++ camera4GGA_AVF2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camera4GGA_AVF2
+//
+//  Created by Chie Takahashi on 2018/03/12.
+//  Copyright © 2018年 ctak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
